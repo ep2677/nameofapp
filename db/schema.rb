852cb07f4170919ea11cfe4e45c66bfee9e828d9
Edward <epwalsh77@gmail.com>
2017-04-14 15:28:02 -0400

    4.1 db updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409042942) do
+ActiveRecord::Schema.define(version: 20170414192217) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.string   "text"
     t.string   "image_url"
-    t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
