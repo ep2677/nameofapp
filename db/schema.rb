0b7d941e487f443d6b3bd4171dda56be510066d7
Edward <epwalsh77@gmail.com>
2017-04-20 21:29:34 -0400

    4.2 images fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418013031) do
+ActiveRecord::Schema.define(version: 20170420021418) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170418013031) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.string   "string"
     t.float    "price"
   end
 
