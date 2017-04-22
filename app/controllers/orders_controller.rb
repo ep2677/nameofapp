38d16ae27adaf50e5019cec881fb16c1bba2bde0
Edward <epwalsh77@gmail.com>
2017-04-22 14:38:16 -0400

    4.3 OrdersController Added, views added

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destryoy
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
 end
 
 
