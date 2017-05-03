3268c8cf0902e8b5f5130229f3c97ec9bc591e1f
Edward <epwalsh77@gmail.com>
2017-05-03 00:52:05 -0400

    5.1 Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+  root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
