f450f9d86ed6dd882a37a62fbbd7ecd62fca0f9b
Edward <epwalsh77@gmail.com>
2017-05-07 16:06:34 -0400

    5.1 products rendering fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,11 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.development? 
+      Product.where("name LIKE ?", "%#{search_term}%")
+   else
+    Product.where("name ilike ?", "%#{search_term}%") 
+    end
   end
+
 end
