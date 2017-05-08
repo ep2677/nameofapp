ca6fba7f75cf390c662afd4fc07d9e006efdcb1f
Edward <epwalsh77@gmail.com>
2017-05-07 22:14:37 -0400

    5.3 email sendgrid setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,13 @@ class StaticPagesController < ApplicationController
     #@featured_product = Product.first
     @products = Product.limit(3)
   end
-  
+  def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      ActionMailer::Base.mail(:from => @email, 
+        :to => 'epwalsh77@gmail.com',
+        :subject => 'A new contact message from #{@name}',
+        :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
   get 'static_pages/landing_page'
+  post 'static_pages/thank_you'
   root 'products#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
