52705bf7ef70f695592a9e5f1758ae4f0f160778
Edward <epwalsh77@gmail.com>
2017-05-14 14:36:02 -0400

    5.4 user model created, devise installed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-
+before_filter :authenticate_user!
   def index
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
-class Order < ApplicationRecord
+class Order < ApplicationRecord 
   belongs_to :product
+  belongs_to :user
 end
