50eeeb91b6a42f563bd3748e81d1b90668a87a44
Edward <epwalsh77@gmail.com>
2017-05-14 20:37:56 -0400

    5.4 production.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,5 +98,5 @@ Rails.application.configure do
   :enable_starttls_auto => true
 }
 
-config.action_mailer.default_url_options = { :host => 'https://bikerberlin77.herokuapp.com'}
+config.action_mailer.default_url_options = { :host => 'https://bikeberlin77.herokuapp.com'}
 end
