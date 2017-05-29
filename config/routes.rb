0dbee24d434c98ac08beba388bef17d9e5998ae1
Edward <epwalsh77@gmail.com>
2017-05-28 22:49:42 -0400

    Style changes, changed root

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   post 'static_pages/thank_you'
   get 'products/index'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
