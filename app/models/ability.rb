69ae7a166945dd4264fb967e517d98a334bd5940
Edward <epwalsh77@gmail.com>
2017-05-30 23:10:52 -0400

    5.10 Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id
-
+    if user.admin?
+      can :destroy, Comment 
+    else
+      can :manage, User, id: user.id
   end
 end
