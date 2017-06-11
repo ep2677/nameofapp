7742ec39136893c0a8c7b26c901060ccd9aa08b2
Edward <epwalsh77@gmail.com>
2017-06-10 23:40:36 -0400

    6.3 functional testing

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
   gem 'rspec-rails', '~> 3.5'
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  before do
+    @user = User.create!(email:"afdafasdf@gmail.com", password:"dafdfa")
+    @user2 = User.create!(email:"User2@gmail.com", password:"djafhdjfhjads")
+  end
+  describe 'GET #show' do
+    context 'when a user is logged in' do
+      before do
+        sign_in @user
+      end
+      it 'loads correct user details' do
+        get :show, params: { id: @user.id }
+        expect(assigns(:user)).to eq @user
+        expect(response).to have_http_status(200)
+      end
+      it 'cant access other users show page' do
+        get :show, params: { id: @user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when a user is not logged in' do
+      it 'redirects to login' do
+        get :show, params: { id: @user.id }
+        expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
+end
