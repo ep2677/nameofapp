e5f8b1b878da4a16dc0848a18949347c2b0b4f3c
Edward <epwalsh77@gmail.com>
2017-07-12 22:33:37 -0400

    6.14 advanced action cable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,9 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     respond_to do |format|
       if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating:
-      @comment.product.average_rating
+        ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
+        #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating:
+      #@comment.product.average_rating
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
         format.js
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, path:  '',
+                     path_names: {:sign_in => 'login', :sign_out => 'logout'},
+                     controllers: {registrations: 'users/registrations'}
   resources :users
   resources :products do
     resources :comments
