ea2b45f3551c4db93b4b56b5195d888b62e6c9fe
Edward <epwalsh77@gmail.com>
2017-07-14 00:39:40 -0400

    6.14 raty method added

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -12,6 +12,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     $(".alert.alert-info").show();
     $('.product-reviews').prepend(data.comment);
     $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
   },
   listen_to_comments: function() {
     return this.perform('listen', {
